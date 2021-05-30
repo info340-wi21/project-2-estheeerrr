d72dc815d9d0e390f5143a8d0ab6382e7121810d
annienobear <shirley757363157@gmail.com>
2021-05-30 11:50:31 +0800

    test

--- src/playerDetail.js
+++ src/playerDetail.js
@@ -1,26 +1,29 @@
 import { React, useState } from 'react';
+import fetch from './fetch';
 import { UncontrolledCarousel, Button } from 'reactstrap';
 import _ from 'lodash';
 import { useParams } from 'react-router';
 
 
-function DetailPage(props) {
+async function DetailPage(props) {
     const [data, setData] = useState(undefined)
     let name = props.name;
-    getInfo(name, setData);
-    
-
+    setData(fetch);
+    console.log(data);
     return <p>{data}</p>;
 }
 
-async function getInfo(name, callback) {
-    console.log(222)
-    fetch('./playerNote.json')
-    .then(res => res.json())
-    .then(function(data) {
-        callback("11111");
-        console.log(data)
+async function getData(setData) {
+    let res = await fetch('http://test449.site44.com/playerNote.json')
+    .then(function(response) {
+        response.json().then(function(data) {
+            setData(data.notes);
+        });
     })
+    .catch(function(err) {
+        console.log(err);
+    });
+
 }
 
 export default DetailPage;
