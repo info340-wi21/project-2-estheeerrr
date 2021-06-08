ae3bc957be6567ccef375c4066ed49cc839d8fbc
Sunny <syfxlichenu@163.com>
2021-06-08 11:29:54 +0800

    add navBar

--- src/App.js
+++ src/App.js
@@ -6,33 +6,58 @@ import { CardBoard } from './playerBoad';
 import { PlayerForm } from './form';
 import { GameBoard } from './gameBoard';
 import { useState } from 'react';
-import { Router } from 'react-dom'
+import { Router, NavLink} from 'react-dom'
 
 function App(props) {
   const [players, setPlayers] = useState(props.player);
   return (
+   
     <div className="App">
+
       <header className="App-header">
         <h1>Player Viewer</h1>
         <p>By Team Cereal Killer</p>
       </header>
+
+
       <main>
-        <div className="container">
-          <CardBoard players={players} />
-        </div>
+        <div className="row">
+          <div className="col-3">
+            <AboutNav />
+          </div>
+          
+          <div className="col-9">
+            <div className="container">
+              <CardBoard players={players} />
+            </div>
+          
+            <div className="container">
+              <PlayerForm />
+            </div>
 
-        <div className="container">
-          <PlayerForm />
-        </div>
-
-        <div className="container">
-          <GameBoard />
-        </div>
-
+            <div className="container">
+              <GameBoard />
+            </div>
+          </div>
+       </div>
       </main>
       <footer>A project for INFO 340 <a href="https://github.com/info340a-sp21/project-01-Gyangle">Link</a></footer>
     </div>
+    
   );
 }
 
+/*Navigation bar that used to redirect routing */
+function AboutNav() {
+  return (
+    <nav id="aboutLinks">
+      <h2>About</h2>
+      <ul className="list-unstyled">
+        <li>PlayerBoard</li>
+        <li>PlayerForm</li>
+        <li>GameBoard</li>
+      </ul>
+    </nav>
+  );
+}
 export default App;
